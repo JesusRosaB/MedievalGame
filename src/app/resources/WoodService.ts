2ee3482dc7ded5d6551008ba4e765d532da6f331
Jose <jsperegrina1@gmail.com>
2017-12-14 18:03:55 +0100

    Poniendo una idea del patron observable

--- src/app/resources/WoodService.ts
+++ src/app/resources/WoodService.ts
@@ -1,8 +1,7 @@
 import {Injectable, OnChanges, OnInit, Output} from '@angular/core';
 import {Resources} from './resources';
-
 @Injectable()
-export class WoodService implements OnInit {
+export class WoodService implements OnInit, OnChanges {
   Wood: Resources;
   currentQuantity() {
     return this.Wood.quantity;
@@ -15,5 +14,9 @@ export class WoodService implements OnInit {
   }
   ngOnInit() {
     this.Wood = new Resources(1, 'Madera', 500);
+  }
+  ngOnChanges() {
+    Rx.Observable.next(this.Wood);
+
   }
 }
