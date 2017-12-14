f1245fa37ac95b8a026e705e7270cd5b54178e10
JesusRosaB <jesu_rosa_bilbao_1996@hotmail.com>
2017-12-14 11:40:30 +0100

    Creación de Routing y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+//ROUTING
+import { APP_ROUTING } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { QuarterComponent } from './quarter/quarter.component';
-import {WoodService} from "./resources/WoodService";
+import { WoodService } from "./resources/WoodService";
+import { MarketComponent } from './market/market.component';
+import { ArmyComponent } from './army/army.component';
+import { ArmoryComponent } from './armory/armory.component';
+import { TownhallComponent } from './townhall/townhall.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ResourcesComponent,
-    QuarterComponent
+    QuarterComponent,
+    MarketComponent,
+    ArmyComponent,
+    ArmoryComponent,
+    TownhallComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [WoodService],
   bootstrap: [AppComponent]
