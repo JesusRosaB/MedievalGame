84a744761dfeadf6e1c3b47134c2aa7277d42a40
Alvaro Duran <alvaroduran.96@gmail.com>
2017-12-21 13:58:27 +0100

    Implementación inicial incompleta de Escaramuza(Quest)

---
+++ codigo_aparte/quest.ts
@@ -0,0 +1,34 @@
+﻿export class Quest {
+    private enemyGroup: ArmyComponent;
+    private questTime: number;
+    private reward: number[];
+    private description: string;
+
+    public getEnemyPower(): number {
+        return this.enemyGroup.getPower();
+    }
+
+    public getDescription(): string {
+        return this.description;
+    }
+
+    public getReward(resource: Resource): number {
+        return this.reward[resource.id()];
+    }
+
+    
+    public getQuestTime(): number {
+      return this.questTime;
+    }
+    public takeQuest(squad: ArmyComponent) {
+        squad.setBusy(true);
+        //Esperar un tiempo
+        //Pongo por ahora una representación de batallas como objetos, porque no sé como vamos a representarlas
+        let questBattle: Battle = new Battle(squad, this.enemyGroup);
+        //Suponiendo una función result() que devuelve 0 si ganas 
+        if (questBattle.result() == 0) {
+            //Añadir la recompensa a los recursos
+            squad.setBusy(false);
+        }
+    }
+}
